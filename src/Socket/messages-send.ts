becf6c4c31701e74d64bc8168a2c25cb46c2eefb
darkdeathoriginal <87226858+darkdeathoriginal@users.noreply.github.com>
2023-05-19 14:25:42 +0530

    test

--- src/Socket/messages-send.ts
+++ src/Socket/messages-send.ts
@@ -289,7 +289,7 @@ export const makeMessagesSocket = (config: SocketConfig) => {
 
 		const { user, server } = jidDecode(jid)!
 		const isGroup = server === 'g.us'
-		msgId = "VIGNESH"+generateMessageID()
+		msgId = generateMessageID()
 		useUserDevicesCache = useUserDevicesCache !== false
 
 		const encodedMsg = encodeWAMessage(message)
